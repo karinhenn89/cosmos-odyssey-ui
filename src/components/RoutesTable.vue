7eece473cde9561445c6b8bdc557b52c6a8b926d
karinhenn <karinhenn89@gmail.com>
2025-01-10 12:17:14 +0200

    select origin from, destination to

--- src/components/RoutesTable.vue
+++ src/components/RoutesTable.vue
@@ -11,24 +11,54 @@
     <br>
   </div>
 
-  <table class="table">
-    <thead>
-    <tr>
-      <th>From Planet</th>
-      <th class="text-center">To Planet</th>
-      <th class="text-center">Distance between</th>
-    </tr>
-    </thead>
+<!--  <table class="table">-->
+<!--    <thead>-->
+<!--    <tr>-->
+<!--      <th>From Planet</th>-->
+<!--      <th class="text-center">To Planet</th>-->
+<!--      <th class="text-center">Distance between</th>-->
+<!--    </tr>-->
+<!--    </thead>-->
 
-    <tbody>
-    <tr v-for="item in fetchRoutes" :key="item">
-      <td>{{ item.from.name }}</td>
-      <td class="text-center">{{  item.to.name }}</td>
-      <td class="text-center">{{  item.distance }}</td>
+<!--    <tbody>-->
+<!--    <tr v-for="item in fetchRoutes" :key="item">-->
+<!--      <td>{{ item.from.name }}</td>-->
+<!--      <td class="text-center">{{  item.to.name }}</td>-->
+<!--      <td class="text-center">{{  item.distance }}</td>-->
 
-    </tr>
-    </tbody>
-  </table>
+<!--    </tr>-->
+<!--    </tbody>-->
+<!--  </table>-->
+
+  <div>
+    <h2>Select Origin and Destination</h2>
+
+    <!-- Dropdown for Origin (fromOptions) -->
+    <label for="from">From:</label>
+    <select v-model="selectedFrom" id="from">
+      <option value="" disabled>Select origin</option>
+      <option v-for="from in fromOptions" :key="from" :value="from">
+        {{ from }}
+      </option>
+    </select>
+
+    <!-- Dropdown for Destination (toOptions) -->
+    <label for="to">To:</label>
+    <select v-model="selectedTo" id="to">
+      <option value="" disabled>Select destination</option>
+      <option v-for="to in toOptions" :key="to" :value="to">
+        {{ to }}
+      </option>
+    </select>
+
+    <!-- Display Selected Values -->
+    <div v-if="selectedFrom && selectedTo" class="selected-info">
+      <p>You selected:</p>
+      <p>From: {{ selectedFrom }}</p>
+      <p>To: {{ selectedTo }}</p>
+    </div>
+  </div>
+
 
 </template>
 
@@ -38,27 +68,57 @@ import axios from "axios";
 
 
 export default {
-data: () =>({
-  routesApi: "http://localhost:8090/api",
-  storeRoute: [],
-  pricelist: '',
-}),
-  methods: {
-    fetchRoutes() {
-      axios.all([
-          axios.get(`${this.routesApi}/routes`).then(res =>(this.storeRoute = res.data)),
-      ]);
+  data: () => ({
+    routesApi: "http://localhost:8090/api",
+    fromOptions: [],
+    toOptions: [],
+    selectedFrom: '',
+    selectedTo: ''
+  }),
+  computed: {
+    // Enable the search button only if both dropdowns have a value
+    canSearch() {
+      return this.selectedFrom && this.selectedTo;
     },
+  },
+    methods: {
+      fetchLocations() {
+        axios.get(`${this.routesApi}/routes/locations`)
+            .then(res => {
 
-    pricelistIsValid() {
-      axios.get(`${this.routesApi}/is-valid`).then(res =>(this.pricelist = res.data));
-    }
-  },
-  mounted() {
-    this.fetchRoutes();
-    this.pricelistIsValid();
-  },
-}
+              // Extract fromOptions and toOptions from the response
+              this.fromOptions = res.data.fromOptions;
+              this.toOptions = res.data.toOptions;
+            })
+            .catch(error => {
+              console.error('Error fetching routes:', error);
+            });
+      },
+      searchRoutes() {
+        // Perform the search logic here
+        console.log("Searching for routes from", this.selectedFrom, "to", this.selectedTo);
+
+        // You can add logic to make another API call for searching routes
+        axios.get(`${this.routesApi}/routes/search`, {
+          params: {
+            fromName: this.selectedFrom,
+            toName: this.selectedTo
+          }
+        })
+            .then(res => {
+              console.log("Search Results:", res.data);
+            })
+            .catch(error => {
+              console.error('Error searching routes:', error);
+            });
+      }
+
+    },
+    mounted() {
+      this.fetchLocations();
+      // this.pricelistIsValid();
+    },
+  }
 
 </script>
 
